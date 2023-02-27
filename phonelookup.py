1c45fec0a45ad4819ed1f1e5a192fe8fea8b3ada
RyeG <ryezorgames@gmail.com>
2023-02-28 11:01:47 +1300

    New commit

--- phonelookup.py
+++ phonelookup.py
@@ -5,3 +5,8 @@ def phonebook_lookup(filename):
             for line in f:
 raw_words = line.split()   phonebook_dict[raw_words[0].strip(unwanted_chars)] = raw_words[1].strip(unwanted_chars)
       return(phonebook_dict)
+
+phonebook=phonebook_lookup("phonebook.txt")
+
+print("\nPrinting your phonebook dict as key-value pair of name-phone")
+print(phonebook_lookup("phonebook.txt"))
